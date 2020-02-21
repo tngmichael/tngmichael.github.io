3dc26a1914b88e2d04080e9ebde83d5f13d7034d
Michael <tngmichael89@gmail.com>
2020-02-21 13:06:16 +0700

    Updates

---
+++ precache-manifest.cc9872e6993ad40291e3a634f2691747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b74cc8dc97afde6da414b95c9a6968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce9ff7895be9d391903f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c29766ef4e5abe690242",
+    "url": "/static/js/2.bf0bad0b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bf0bad0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9ff7895be9d391903f",
+    "url": "/static/js/main.6847dfed.chunk.js"
+  },
+  {
+    "revision": "c0c6d8d040734ec73ab3",
+    "url": "/static/js/runtime-main.12aa758c.js"
+  }
+]);
