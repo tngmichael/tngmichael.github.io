5bb66593acb36069b6010d85e6e728d3dca2c4e8
Michael <tngmichael89@gmail.com>
2020-06-03 10:44:28 +0700

    Add discord link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header className="App-header">
         MAIKL CAMP<br/>
         <i style={{ fontSize: 12 }}>under construction..</i>
+        <div style={{ marginTop: 8 }}>
+          <a href="https://discord.gg/BFzZ2Jx" className="Discord-link">
+            Click here to join my Discord
+          </a>
+        </div>
       </header>
     </div>
   );
